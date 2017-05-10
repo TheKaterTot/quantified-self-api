c06a08a8fac29ad3131546ac7018997373275ee7
Casey Macaulay <caseyannemacaulay@gmail.com>
2017-05-10 13:46:49 -0600

    add tests for dinner, lunch and snacks routes

--- test/server-test.js
+++ test/server-test.js
@@ -205,4 +205,126 @@ describe('Server', () => {
       })
     })
   })
+
+  describe('GET /api/meals/lunch/<date>', () => {
+    beforeEach((done) => {
+      Food.create('taco', 250)
+      .then((data) => {
+        this.food = data.rows[0];
+        return database.raw(
+          'INSERT INTO categories (name, created_at) VALUES (?, ?) RETURNING *',
+          ['lunch', new Date])
+      }).then((data) => {
+        this.category = data.rows[0];
+        return database.raw(
+          'INSERT INTO meals (food_id, category_id, date, created_at) VALUES (?, ?, ?, ?)',
+          [this.food.id, this.category.id, new Date(2015, 15, 11), new Date])
+      }).then(() => {
+        done();
+      }).catch(done)
+    })
+
+    afterEach(truncate)
+
+    it('should return the meal data', (done) => {
+      this.request.get(`/api/meals/lunch/2015/15/11`, (err, res) => {
+        assert.equal(res.statusCode, 200);
+
+        const parsedData = JSON.parse(res.body);
+
+        assert.equal(parsedData.length, 1)
+        assert.equal(parsedData[0].food_id, this.food.id)
+        assert.equal(parsedData[0].category_id, this.category.id)
+        assert.equal(parsedData[0].food_name, 'taco')
+        assert.equal(parsedData[0].calories, 250)
+        assert.equal(parsedData[0].category_name, 'lunch')
+        done();
+      })
+    })
+  })
+
+  describe('GET /api/meals/dinner/<date>', () => {
+    beforeEach((done) => {
+      Food.create('calzone', 2500)
+      .then((data) => {
+        this.food = data.rows[0];
+        return database.raw(
+          'INSERT INTO categories (name, created_at) VALUES (?, ?) RETURNING *',
+          ['dinner', new Date])
+      }).then((data) => {
+        this.category = data.rows[0];
+        return database.raw(
+          'INSERT INTO meals (food_id, category_id, date, created_at) VALUES (?, ?, ?, ?)',
+          [this.food.id, this.category.id, new Date(2017, 02, 02), new Date])
+      }).then(() => {
+        done();
+      }).catch(done)
+    })
+
+    afterEach(truncate)
+
+    it('should return the meal data', (done) => {
+      this.request.get(`/api/meals/dinner/2017/02/02`, (err, res) => {
+        assert.equal(res.statusCode, 200);
+
+        const parsedData = JSON.parse(res.body);
+
+        assert.equal(parsedData.length, 1)
+        assert.equal(parsedData[0].food_id, this.food.id)
+        assert.equal(parsedData[0].category_id, this.category.id)
+        assert.equal(parsedData[0].food_name, 'calzone')
+        assert.equal(parsedData[0].calories, 2500)
+        assert.equal(parsedData[0].category_name, 'dinner')
+        done();
+      })
+    })
+  })
+
+  describe('GET /api/meals/snacks/<date>', () => {
+    beforeEach((done) => {
+      Food.create('popcorn', 10)
+      .then((data) => {
+        this.food = data.rows[0];
+        return database.raw(
+          'INSERT INTO categories (name, created_at) VALUES (?, ?) RETURNING *',
+          ['snacks', new Date])
+      }).then((data) => {
+        this.category = data.rows[0];
+        return database.raw(
+          'INSERT INTO meals (food_id, category_id, date, created_at) VALUES (?, ?, ?, ?)',
+          [this.food.id, this.category.id, new Date(2001, 12, 20), new Date])
+      }).then(() => {
+        done();
+      }).catch(done)
+    })
+
+    afterEach(truncate)
+
+    it('should return the meal data', (done) => {
+      this.request.get(`/api/meals/snacks/2001/12/20`, (err, res) => {
+        assert.equal(res.statusCode, 200);
+
+        const parsedData = JSON.parse(res.body);
+
+        assert.equal(parsedData.length, 1)
+        assert.equal(parsedData[0].food_id, this.food.id)
+        assert.equal(parsedData[0].category_id, this.category.id)
+        assert.equal(parsedData[0].food_name, 'popcorn')
+        assert.equal(parsedData[0].calories, 10)
+        assert.equal(parsedData[0].category_name, 'snacks')
+        done();
+      })
+    })
+
+    it('returns an empty array if there is no snack for that day', (done) =>{
+      this.request.get(`/api/meals/snacks/2001/02/20`, (err, res) => {
+        assert.equal(res.statusCode, 200);
+
+        const parsedData = JSON.parse(res.body);
+
+        assert.equal(parsedData.length, 0)
+        done();
+      })
+    })
+  })
 })
