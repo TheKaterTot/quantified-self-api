45e30ae21a85de2772131f7820ff1779e7db94c8
Casey Macaulay <caseyannemacaulay@gmail.com>
2017-05-10 14:49:52 -0600

    add extracted route files

--- server.js
+++ server.js
@@ -1,25 +1,19 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
-const FoodsController = require('./lib/controllers/foods-controller');
-const MealsController = require('./lib/controllers/meals-controller');
-const Food = require('./lib/models/food');
+const foodsRouter = require('./lib/routers/foods-router');
+const mealsRouter = require('./lib/routers/meals-router');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('port', port);
-
 app.listen(port, function () {
   console.log(`Listening on port ${port}!`);
 });
 
-app.get('/api/foods', FoodsController.index);
-app.post('/api/foods', FoodsController.create);
-app.patch('/api/foods/:id', FoodsController.update);
-app.delete('/api/foods/:id', FoodsController.destroy);
-
-app.get('/api/meals/:category/:year/:month/:day', MealsController.index);
+app.use('/api/foods/', foodsRouter);
+app.use('/api/meals/', mealsRouter);
 
 module.exports = app;
