9b7df72f4052afc3ed7b51f8f19f6a25ade89537
Katy Farmer <dreadpiratekaty@gmail.com>
2017-05-13 18:50:05 -0600

    Update seed data

--- db/seeds/production/meals.js
+++ db/seeds/production/meals.js
@@ -4,23 +4,43 @@ exports.seed = function(knex, Promise) {
       return Promise.all([
         knex.raw(
           'INSERT INTO meals (date, category_id, food_id, created_at) VALUES (?, ?, ?, ?)',
-          [new Date(2017, 04, 11), 1, 3, new Date]
+          [new Date, 1, 3, new Date]
         ),
         knex.raw(
           'INSERT INTO meals (date, category_id, food_id, created_at) VALUES (?, ?, ?, ?)',
-          [new Date(2017, 04, 11), 2, 3, new Date]
+          [new Date, 1, 2, new Date]
         ),
         knex.raw(
           'INSERT INTO meals (date, category_id, food_id, created_at) VALUES (?, ?, ?, ?)',
-          [new Date(2017, 04, 11), 3, 3, new Date]
+          [new Date, 2, 3, new Date]
         ),
         knex.raw(
           'INSERT INTO meals (date, category_id, food_id, created_at) VALUES (?, ?, ?, ?)',
-          [new Date(2017, 04, 11), 4, 3, new Date]
+          [new Date, 2, 3, new Date]
+        ),
+        knex.raw(
+          'INSERT INTO meals (date, category_id, food_id, created_at) VALUES (?, ?, ?, ?)',
+          [new Date, 3, 3, new Date]
+        ),
+        knex.raw(
+          'INSERT INTO meals (date, category_id, food_id, created_at) VALUES (?, ?, ?, ?)',
+          [new Date, 4, 3, new Date]
+        ),
+        knex.raw(
+          'INSERT INTO meals (date, category_id, food_id, created_at) VALUES (?, ?, ?, ?)',
+          [new Date, 4, 1, new Date]
         ),
         knex.raw(
           'INSERT INTO meals (date, category_id, food_id, created_at) VALUES (?, ?, ?, ?)',
           [new Date(2017, 04, 10), 1, 3, new Date]
+        ),
+        knex.raw(
+          'INSERT INTO meals (date, category_id, food_id, created_at) VALUES (?, ?, ?, ?)',
+          [new Date(2017, 04, 10), 1, 1, new Date]
+        ),
+        knex.raw(
+          'INSERT INTO meals (date, category_id, food_id, created_at) VALUES (?, ?, ?, ?)',
+          [new Date(2017, 04, 10), 2, 3, new Date]
         ),
         knex.raw(
           'INSERT INTO meals (date, category_id, food_id, created_at) VALUES (?, ?, ?, ?)',
@@ -32,7 +52,15 @@ exports.seed = function(knex, Promise) {
         ),
         knex.raw(
           'INSERT INTO meals (date, category_id, food_id, created_at) VALUES (?, ?, ?, ?)',
+          [new Date(2017, 04, 10), 3, 1, new Date]
+        ),
+        knex.raw(
+          'INSERT INTO meals (date, category_id, food_id, created_at) VALUES (?, ?, ?, ?)',
           [new Date(2017, 04, 10), 4, 3, new Date]
+        ),
+        knex.raw(
+          'INSERT INTO meals (date, category_id, food_id, created_at) VALUES (?, ?, ?, ?)',
+          [new Date(2017, 04, 10), 4, 1, new Date]
         ),
         knex.raw(
           'INSERT INTO meals (date, category_id, food_id, created_at) VALUES (?, ?, ?, ?)',
@@ -40,7 +68,19 @@ exports.seed = function(knex, Promise) {
         ),
         knex.raw(
           'INSERT INTO meals (date, category_id, food_id, created_at) VALUES (?, ?, ?, ?)',
+          [new Date(2017, 04, 12), 1, 3, new Date]
+        ),
+        knex.raw(
+          'INSERT INTO meals (date, category_id, food_id, created_at) VALUES (?, ?, ?, ?)',
+          [new Date(2017, 04, 12), 2, 1, new Date]
+        ),
+        knex.raw(
+          'INSERT INTO meals (date, category_id, food_id, created_at) VALUES (?, ?, ?, ?)',
           [new Date(2017, 04, 12), 2, 3, new Date]
+        ),
+        knex.raw(
+          'INSERT INTO meals (date, category_id, food_id, created_at) VALUES (?, ?, ?, ?)',
+          [new Date(2017, 04, 12), 3, 2, new Date]
         ),
         knex.raw(
           'INSERT INTO meals (date, category_id, food_id, created_at) VALUES (?, ?, ?, ?)',
@@ -52,7 +92,15 @@ exports.seed = function(knex, Promise) {
         ),
         knex.raw(
           'INSERT INTO meals (date, category_id, food_id, created_at) VALUES (?, ?, ?, ?)',
+          [new Date(2017, 04, 12), 4, 1, new Date]
+        ),
+        knex.raw(
+          'INSERT INTO meals (date, category_id, food_id, created_at) VALUES (?, ?, ?, ?)',
           [new Date(2017, 04, 13), 1, 3, new Date]
+        ),
+        knex.raw(
+          'INSERT INTO meals (date, category_id, food_id, created_at) VALUES (?, ?, ?, ?)',
+          [new Date(2017, 04, 13), 1, 1, new Date]
         ),
         knex.raw(
           'INSERT INTO meals (date, category_id, food_id, created_at) VALUES (?, ?, ?, ?)',
@@ -60,7 +108,15 @@ exports.seed = function(knex, Promise) {
         ),
         knex.raw(
           'INSERT INTO meals (date, category_id, food_id, created_at) VALUES (?, ?, ?, ?)',
+          [new Date(2017, 04, 13), 2, 2, new Date]
+        ),
+        knex.raw(
+          'INSERT INTO meals (date, category_id, food_id, created_at) VALUES (?, ?, ?, ?)',
           [new Date(2017, 04, 13), 3, 3, new Date]
+        ),
+        knex.raw(
+          'INSERT INTO meals (date, category_id, food_id, created_at) VALUES (?, ?, ?, ?)',
+          [new Date(2017, 04, 13), 3, 1, new Date]
         ),
         knex.raw(
           'INSERT INTO meals (date, category_id, food_id, created_at) VALUES (?, ?, ?, ?)',
@@ -68,7 +124,15 @@ exports.seed = function(knex, Promise) {
         ),
         knex.raw(
           'INSERT INTO meals (date, category_id, food_id, created_at) VALUES (?, ?, ?, ?)',
-          [new Date(2017, 04, 14), 1, 1, new Date]
+          [new Date(2017, 04, 14), 1, 3, new Date]
+        ),
+        knex.raw(
+          'INSERT INTO meals (date, category_id, food_id, created_at) VALUES (?, ?, ?, ?)',
+          [new Date(2017, 04, 14), 2, 1, new Date]
+        ),
+        knex.raw(
+          'INSERT INTO meals (date, category_id, food_id, created_at) VALUES (?, ?, ?, ?)',
+          [new Date(2017, 04, 14), 2, 3, new Date]
         )
       ])
     })
